383ebe798e8ab0240b5979f45ad899f760daa938
rulamon <ruben.lamon@gmail.com>
2018-11-30 21:10:03 +0100

    used a function for the sum for new number in fib sequence

--- 1.js
+++ 1.js
@@ -1,8 +1,10 @@
 function sumFibs(num) {
-	//generate fibonacci <= num
-	let arrFibs = [1, 1];
-	for (let i = 1; arrFibs[i-1] + arrFibs[i] <= num; i++) {
-		arrFibs.push(arrFibs[i-1] + arrFibs[i])
+	//define function for new number in fibonacci sequence
+	const sum = (arr, index) => arr[index] + arr[index - 1];
+	//generate fibonacci sequence with last number <= num
+	let arrFibs = [1, 1]; 
+	for (let i = 1; sum(arrFibs, i) <= num; i++) {
+		arrFibs.push(sum(arrFibs, i))
 	}
 	//sum of odd numbers
 	return arrFibs.reduce((a, b) => b % 2 == 1 ? a + b : a)
